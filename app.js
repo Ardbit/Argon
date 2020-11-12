3817fe806e5d55da43125025f4116bb5c6881239
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-12 17:31:34 +1000

    Add project files.

---
+++ app.js
@@ -0,0 +1,27 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+var bot = new Discord.Client({
+    token: process.env.DISCORD_TOKEN,
+    autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    logger.info('msg');
+    bot.sendMessage({
+        to: channelID,
+        message: 'I am a bot'
+    });
+});
