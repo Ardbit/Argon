6b32b7baceeb6c70b51b9563d38cfc77339eb831
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-14 16:48:40 +1000

    Fix DiscordAPIError: Unknown Message in .clear

--- utils/messages.js
+++ utils/messages.js
@@ -17,7 +17,7 @@ function ArgonSuccess(message, description, remove = false, timeout = 3000) {
         .setDescription(description)
         .setColor(0x38A700)).then(msg => {
             if (remove) {
-                msg.delete(timeout)
+                msg.delete({timeout})
             }
         });
 }
