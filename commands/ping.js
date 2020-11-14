30cbe2e066ecd11678aa479287513777d5b7d164
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-14 12:48:06 +1000

    Final Adjustments to ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,16 +2,14 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (client, message, args, logger) => {
     message.channel.send(new MessageEmbed()
-        .setTitle(':ping_pong: **Pong!**')
+        .setTitle(':ping_pong:   **Pong!**')
         .setColor(0x31CB00)
         .setDescription('**Pinging...**')
-        .setFooter('Argon')
     ).then(msg => {
         msg.edit(new MessageEmbed()
-            .setTitle(':ping_pong: **Pong!**')
+            .setTitle(':ping_pong:   **Pong!**')
             .setColor(0x31CB00)
-            .setDescription(`**Latency**\n${message.createdTimestamp - Date.now()}\n\n**API**\n${client.ws.ping}`)
-            .setFooter('Argon')
+            .setDescription(`**Latency**\n${Date.now() - message.createdTimestamp}ms\n\n**API**\n${client.ws.ping}ms`)
         );
     });
 }
