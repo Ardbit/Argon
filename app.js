1ce1d0066420816d2cf9207afbfdcb1355f07bba
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-14 11:36:24 +1000

    added logger.debug(config)

--- app.js
+++ app.js
@@ -128,7 +128,8 @@ client.on('message', async (message) => {
     const cmd = messageArray[0];
     const args = messageArray.slice(1);
 
-    if (!message.content.startsWith(config.prefix)) return;
+    //if(!message.content.startsWith(config.prefix)) return;
+    logger.debug(config)
 
     const commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)));
     commandfile.run(client, message, args, logger);
