d4859e68e4aa61b97fb5e2d0c7b9f77ea1e8be2a
Lachlan Chambers <67294530+Ardbit@users.noreply.github.com>
2020-11-15 16:57:40 +1000

    Rewrote warn command.

--- commands/warn.js
+++ commands/warn.js
@@ -1,130 +1,73 @@
-const { ArgonError, ArgonSuccess } = require('../utils/messages');
-const { database, logger } = require('../utils/defaults');
-const { MessageEmbed } = require('discord.js');
-const defaults = require('../utils/defaults');
+const { ArgonError, ArgonSuccess } = require('../utils/messages')
+const { database, logger } = require('../utils/defaults')
+const defaults = require('../utils/defaults')
 
-module.exports.run = async (message, args) => {
-    try {
-        if (!message.member.hasPermission('KICK_MEMBERS')) {
-            ArgonError(message, 'Insufficient privileges.', true)
-        }
+module.exports.run = async (message, args, logger) => {
+    if (!message.member.hasPermission('KICK_MEMBERS')) {
+        ArgonError(message, 'Invalid permissions.', true);
+        return;
+    }
 
-        const user = message.mentions.members.first();
+    const user = message.mentions.members.first();
+    const guild = message.guild;
 
-        if (!user) {
-            ArgonError(message, 'User does not exist.', true)
-        }
+    let reason = ' ';
 
-        let reason = ' ';
+    for (let i = 1; i < args.length; i++) {
+        reason = reason + args[i] + ' '
+    }
 
-        for (let i = 1; i < args.length; i++) {
-            reason = reason + args[i] + ' '
-        }
+    if (!user) {
+        ArgonError(message, 'Invalid Syntax:\nYou need to mention a user.', true)
+    }
 
-        let warn, result = await database.connect((error, client, done) => {
-            if (error) {
-                logger.error(error);
-                return;
-            }
-
-            client.query('INSERT INTO guilds (id, config, plugins) VALUES ($1, $2, $3) ON CONFLICT DO NOTHING', [message.guild.id, defaults.CONFIG_JSON_DB, defaults.PLUGIN_JSON_DB], (error, result) => {
-                if (error) {
-                    logger.error(error);
-                    return;
-                }
-
-                return result.rows;
-            })
-
-            client.query('COMMIT', (error, result) => {
-                if (error) {
-                    logger.error(error);
-                    return;
-                }
-
-                return result.rows;
-            })
-
-            let warn, result = client.query('SELECT plugins FROM guilds WHERE id = $1', [message.guild.id], (error, result) => {
-                if (error) {
-                    logger.error(error);
-                    return;
-                }
-
-                return result.rows[0]['plugins']['moderation']['warns'], result;
-                done();
-            });
-
-            return warn, result;
-        });
-
-        console.warn(result)
-
-        try {
-            if (warn[user.id]) {
-                warn[user.id][warn[user.id].length || 0] = {
-                    timestamp: Date.now(),
-                    reason: reason || null,
-                    issuer: message.member.id || null
-                }
-            }
-        } catch (error) {
+    const warnCount = await database.connect((error, client, done) => {
+        if (error) {
             logger.error(error)
-            warn[user.id] = [{
-                timestamp: Date.now(),
-                reason: reason || null,
-                issuer: message.member.id || null
-            }]
+            ArgonError(message, 'A database error has occured.')
         }
 
-        database.connect((error, client, done) => {
+        let warns = client.query('SELECT * FROM guilds WHERE id = $1', [guild.id], (error, result) => {
             if (error) {
                 logger.error(error)
-                return
+                ArgonError(message, 'A database error has occured.')
             }
 
-            if (warn == null || warn.length <= 0) {
-                logger.error('Warn is null or less than 0');
-                return;
-            }
-
-            client.query('UPDATE guilds SET plugins = $1 WHERE id = $2', [warn, message.guild.id], (error, result) => {
-                if (error) {
-                    logger.error(error);
-                    return;
-                }
-
-                return result.rows;
-            })
-
-            client.query('COMMIT', (error, result) => {
-                if (error) {
-                    logger.error(error);
-                    return;
-                }
-
-                return result.rows;
-            })
-
-            done();
+            return result.rows[0].plugins.moderation.warns;
         })
 
-        let warnCount = warn[user.id].length + 1;
+        if (!warns[user.id]) {
+            warns[user.id] = []
+        }
 
-        ArgonSuccess(message, `Successfully warned user ${user.tag}`, true);
+        warns[user.id][warns[user.id].length] = {
+            reason,
+            timestamp: Date.now(),
+            issuer: message.member.id
+        }
 
-        message.channel.send(`<@${user.id}>\n`)
-        message.channel.send(new MessageEmbed()
-            .setTitle(`Hey, ${user}!`)
-            .setColor(0x63B0CD)
-            .setDescription(`You have been warned by ${message.author.tag}.`)
-            .addField('Reason', reason || 'None')
-            .setFooter(`Warning count: ${warnCount}`)
-        )
-    } catch (error) {
-        logger.error(error)
-        ArgonError(message, 'Something went wrong.')
-    }
+        client.query('UPDATE guilds SET plugins = $1 WHERE id = $1', [warns, guild.id], (error, result) => {
+            if (error) {
+                logger.error(error)
+                ArgonError(message, 'A database error has occured.')
+            }
+
+            return result.rows[0];
+        })
+
+        return warns
+    })[user.id].length;
+
+    ArgonSuccess(message, 'Successfully warned ' + user.tag)
+
+    message.channel.send(`<@${user.id}>`)
+    message.channel.send(new MessageEmbed()
+        .setColor(0x63B0CD)
+        .setTitle(`Hey, ${user.tag}`)
+        .setDescription('You have been warned by a moderator.')
+        .addField({ name: 'Reason', value: reason || 'None' })
+        .setFooter(`Warning Given: ${warnCount}`)
+    )
 }
 
 module.exports.config = {
